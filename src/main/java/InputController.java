d0e5f30312e31bf1c13f5136ea5f7e8ffbded3ce
satonin18 <satonin18@mail.ru>
2019-09-06 16:10:32 +0500

    start work

---
+++ src/main/java/InputController.java
@@ -0,0 +1,22 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller("inputController")
+//@RequestMapping("/")
+public class InputController {
+
+
+    @GetMapping("/")
+    public String redirectOnMainPage() {
+        return "inputForm";
+    }
+}
+
---
+++ src/main/java/config/security/SecurityWebApplicationInit.java
@@ -0,0 +1,7 @@
+package config.security;//package com.lanit.satonin18.config.security;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SecurityWebApplicationInit
+//        extends AbstractSecurityWebApplicationInitializer{
+//}
